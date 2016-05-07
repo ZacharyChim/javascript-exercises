e2a10f6da82b05016f8a21d802d03fe5510b16ca
zack <zecharyw@gmail.com>
2016-05-07 15:29:05 +0800

    Add CheckList exercise

--- README.md
+++ README.md
@@ -9,3 +9,18 @@
 
 * document.querySelectorAll("tag");
 * this.attributes["data-img"].value;
+
+### 3. checkList
+
+* items[i].addEventListener("click", editItem);
+* inputs[i].addEventListener("blur", updateItem);
+* inputs[i].addEventListener("keypress", itemKeypress);
+* input.focus();
+* input.*setSelectionRange(0, input.value.length)*;
+* this.*previousSibling*.innerHTML = this.value;
+* this.*parentNode*.className = "";
+* function itemKeypress(*event*) {
+  if (*event.which* === 13) {
+    updateItem.*call(this)*;
+  }
+}
---
+++ assets/js/3-checkList.js
@@ -0,0 +1,28 @@
+var checkList = document.getElementById("checklist");
+
+var items = checkList.querySelectorAll("li");
+var inputs = checkList.querySelectorAll("input");
+
+for (var i = 0; i < items.length; i++) {
+  items[i].addEventListener("click", editItem);
+  inputs[i].addEventListener("blur", updateItem);
+  inputs[i].addEventListener("keypress", itemKeypress);
+}
+
+function editItem() {
+  this.className = "edit";
+  var input = this.querySelector("input");
+  input.focus();
+  input.setSelectionRange(0, input.value.length);
+}
+
+function updateItem() {
+  this.previousSibling.innerHTML = this.value;
+  this.parentNode.className = "";
+}
+
+function itemKeypress(event) {
+  if (event.which === 13) {
+    updateItem.call(this);
+  }
+}
